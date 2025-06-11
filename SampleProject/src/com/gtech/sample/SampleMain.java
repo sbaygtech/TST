86242d72998c6a04b5a28ef774df2dfcd1dc9731
GTECH\GTUSR0386 <senol.bayram@gtech.com.tr>
2025-06-11 10:58:14 +0300

    fgfdgs

--- SampleProject/src/com/gtech/sample/SampleMain.java
+++ SampleProject/src/com/gtech/sample/SampleMain.java
@@ -3,11 +3,8 @@ package com.gtech.sample;
 public class SampleMain {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("deneme679");
-=======
 		System.out.println("deneme15");
->>>>>>> dev
 
 	}
 
