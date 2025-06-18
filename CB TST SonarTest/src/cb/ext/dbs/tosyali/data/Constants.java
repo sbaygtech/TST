bea305bb331f9abe0c57a1020d8faf6c71662448
GTECH\GTUSR0386 <senol.bayram@gtech.com.tr>
2025-06-18 10:52:19 +0300

    commit

--- CB TST SonarTest/src/cb/ext/dbs/tosyali/data/Constants.java
+++
@@ -1,8 +0,0 @@
-package cb.ext.dbs.tosyali.data;
-
-public class Constants {
-
-	public static final String INTERNAL_TRY_CURRENCY_CODE = "120.01.";
-	public static final String INTERNAL_USD_CURRENCY_CODE = "120.05.";
-
-}
