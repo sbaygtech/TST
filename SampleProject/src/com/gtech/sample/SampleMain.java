6f646721e4c2614c2a55261f2178ad1de5792c96
GTECH\GTUSR0386 <senol.bayram@gtech.com.tr>
2025-07-01 11:37:58 +0300

    deneme

--- SampleProject/src/com/gtech/sample/SampleMain.java
+++ SampleProject/src/com/gtech/sample/SampleMain.java
@@ -4,7 +4,7 @@ public class SampleMain {
 
 	public static void main(String[] args) {
 		System.out.println("deneme679");
-		System.out.println("deneme15");
+		System.out.println("deneme16");
 
 	}
 
