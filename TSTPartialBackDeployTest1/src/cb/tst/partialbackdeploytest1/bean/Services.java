f7d5863caab3026efa4f0f6cf77924cc44db8b12
GTECH\GTUSR0386 <senol.bayram@gtech.com.tr>
2025-07-04 15:13:18 +0300

    deneme

--- TSTPartialBackDeployTest1/src/cb/tst/partialbackdeploytest1/bean/Services.java
+++ TSTPartialBackDeployTest1/src/cb/tst/partialbackdeploytest1/bean/Services.java
@@ -11,7 +11,7 @@ import cb.smg.general.utility.CBSMGExceptions;
 public class Services implements CBBagKeys {
 
 	/**
-	 * @servicename URN_PARTIALBACKDEPLOYTEST1_LIST_ALL_TABLESsss
+	 * @servicename URN_PARTIALBACKDEPLOYTEST1_LIST_ALL_TABLESssss
 	 * @param inBag
 	 * @return
 	 * @throws CBException
